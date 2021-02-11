f44bba42271d4783f0b87e790e3bff6584b2ca68
muellerdo <domey038@live.de>
2021-02-11 23:41:24 +0100

    Removed unnecessary commented code

--- aucmedi/ensembler/aggregate/majority_vote.py
+++ aucmedi/ensembler/aggregate/majority_vote.py
@@ -55,17 +55,3 @@ class Majority_Vote(Aggregate_Base):
         pred[majority_vote] = 1
         # Return prediction
         return pred
-
-
-        # # Split data columns into multi level structure based on architecutre
-        # data.columns = data.columns.str.split('_', expand=True)
-        # # Identify argmax (vote) for each architecutre and cache n-architectures
-        # data = data.groupby(level=0, axis=1).idxmax(axis=1)
-        # n_architectures = len(data.columns)
-        # data = data.apply(lambda entry: [tup[1] for tup in entry])
-        # # Sum up votes of all architectures
-        # data = data.apply(pd.Series.value_counts, axis=1).fillna(0)
-        # # Compute Class Probabilities
-        # pred = data.divide(n_architectures)
-        # # Return prediction
-        # return pred.to_numpy()

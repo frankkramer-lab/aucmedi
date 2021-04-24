0d851cbd4c169991ac760dbac6b8818a0138b1a1
muellerdo <domey038@live.de>
2021-04-24 13:12:14 +0200

    Updated xai __init__

--- aucmedi/xai/__init__.py
+++ aucmedi/xai/__init__.py
@@ -16,3 +16,10 @@
 #  You should have received a copy of the GNU General Public License           #
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #==============================================================================#
+# Import XAI methods
+from aucmedi.xai.gradcam import GradCAM
+
+# XAI method dictionary
+xai_dict = {
+    "gradcam": GradCAM
+}

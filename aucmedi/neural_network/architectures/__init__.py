f0b0d380b7a1b867b31ba9074f4f8e3d2d9052de
muellerdo <domey038@live.de>
2022-01-26 23:25:12 +0100

    Updated copyright

--- aucmedi/neural_network/architectures/__init__.py
+++ aucmedi/neural_network/architectures/__init__.py
@@ -1,6 +1,6 @@
 #==============================================================================#
 #  Author:       Dominik Müller                                                #
-#  Copyright:    2021 IT-Infrastructure for Translational Medical Research,    #
+#  Copyright:    2022 IT-Infrastructure for Translational Medical Research,    #
 #                University of Augsburg                                        #
 #                                                                              #
 #  This program is free software: you can redistribute it and/or modify        #

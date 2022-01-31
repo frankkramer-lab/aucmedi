e997f44c493212ee6bae1e6c6276f2a22957ed30
muellerdo <domey038@live.de>
2022-01-31 23:39:44 +0100

    Started working on volume augmentation

--- aucmedi/data_processing/augmentation/__init__.py
+++ aucmedi/data_processing/augmentation/__init__.py
@@ -17,3 +17,4 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #==============================================================================#
 from aucmedi.data_processing.augmentation.aug_image import Image_Augmentation
+from aucmedi.data_processing.augmentation.aug_volume import Volume_Augmentation
--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -22,8 +22,9 @@
 #External libraries
 import unittest
 import numpy as np
+import random
 #Internal libraries
-from aucmedi import Image_Augmentation
+from aucmedi import Image_Augmentation, Volume_Augmentation
 
 #-----------------------------------------------------#
 #             Unittest: Image Augmentation            #
@@ -32,42 +33,93 @@ class AugmentationTEST(unittest.TestCase):
     # Create random imaging and classification data
     @classmethod
     def setUpClass(self):
+        random.seed(1234)
         np.random.seed(1234)
         # Create Grayscale data
-        img_gray = np.random.rand(16, 16, 1) * 254
-        self.imgGRAY = np.float32(img_gray)
+        img_gray_2D = np.random.rand(16, 16, 1) * 254
+        self.imgGRAY2d = np.float32(img_gray_2D)
+        img_gray_3D = np.random.rand(16, 16, 16, 1) * 254
+        self.imgGRAY3d = np.float32(img_gray_3D)
         # Create RGB data
-        img_rgb = np.random.rand(16, 16, 3) * 254
-        self.imgRGB = np.float32(img_rgb)
+        img_rgb_2D = np.random.rand(16, 16, 3) * 254
+        self.imgRGB2d = np.float32(img_rgb_2D)
+        img_rgb_3D = np.random.rand(16, 16, 16, 3) * 254
+        self.imgRGB3d = np.float32(img_rgb_3D)
 
     #-------------------------------------------------#
-    #                Base Functionality               #
+    #               Image Functionality               #
     #-------------------------------------------------#
     # Class Creation
-    def test_BASE_create(self):
+    def test_IMAGE_create(self):
         data_aug = Image_Augmentation()
         self.assertIsInstance(data_aug, Image_Augmentation)
 
     # Application
-    def test_BASE_application(self):
-        data_aug = Image_Augmentation()
-        img_augGRAY = data_aug.apply(self.imgGRAY)
-        img_augRGB= data_aug.apply(self.imgRGB)
-        self.assertFalse(np.array_equal(img_augGRAY, self.imgGRAY))
-        self.assertFalse(np.array_equal(img_augRGB, self.imgRGB))
+    def test_IMAGE_application(self):
+        data_aug = Image_Augmentation(flip=True, rotate=True,
+                     brightness=True, contrast=True, saturation=True,
+                     hue=True, scale=True, crop=True, grid_distortion=True,
+                     compression=True, gaussian_noise=True,
+                     gaussian_blur=True, downscaling=True, gamma=True,
+                     elastic_transform=True)
+        data_aug.aug_crop_shape = (8, 8)
+        data_aug.build()
+        img_augGRAY = data_aug.apply(self.imgGRAY2d)
+        img_augRGB = data_aug.apply(self.imgRGB2d)
+        self.assertFalse(np.array_equal(img_augGRAY, self.imgGRAY2d))
+        self.assertFalse(np.array_equal(img_augRGB, self.imgRGB2d))
 
     # Rebuild Augmentation Operator
-    def test_BASE_rebuild(self):
+    def test_IMAGE_rebuild(self):
         data_aug = Image_Augmentation(flip=False, rotate=False,
                      brightness=False, contrast=False, saturation=False,
                      hue=False, scale=False, crop=False, grid_distortion=False,
                      compression=False, gaussian_noise=False,
                      gaussian_blur=False, downscaling=False, gamma=False,
                      elastic_transform=False)
-        img_augRGB = data_aug.apply(self.imgRGB)
-        self.assertTrue(np.array_equal(img_augRGB, self.imgRGB))
+        img_augRGB = data_aug.apply(self.imgRGB2d)
+        self.assertTrue(np.array_equal(img_augRGB, self.imgRGB2d))
         data_aug.aug_flip = True
         data_aug.aug_flip_p = 1.0
         data_aug.build()
-        img_augRGB = data_aug.apply(self.imgRGB)
-        self.assertFalse(np.array_equal(img_augRGB, self.imgRGB))
+        img_augRGB = data_aug.apply(self.imgRGB2d)
+        self.assertFalse(np.array_equal(img_augRGB, self.imgRGB2d))
+
+    #-------------------------------------------------#
+    #               Volume Functionality              #
+    #-------------------------------------------------#
+    # Class Creation
+    def test_VOLUME_create(self):
+        data_aug = Volume_Augmentation()
+        self.assertIsInstance(data_aug, Volume_Augmentation)
+
+    # Application
+    def test_VOLUME_application(self):
+        data_aug = Volume_Augmentation(flip=True, rotate=True,
+                     brightness=True, contrast=True, saturation=True,
+                     hue=True, scale=True, crop=True, grid_distortion=True,
+                     compression=True, gaussian_noise=True,
+                     gaussian_blur=True, downscaling=True, gamma=True,
+                     elastic_transform=True)
+        data_aug.aug_crop_shape = (8, 8, 8)
+        data_aug.build()
+        img_augGRAY = data_aug.apply(self.imgGRAY3d)
+        img_augRGB = data_aug.apply(self.imgRGB3d)
+        self.assertFalse(np.array_equal(img_augGRAY, self.imgGRAY3d))
+        self.assertFalse(np.array_equal(img_augRGB, self.imgRGB3d))
+
+    # Rebuild Augmentation Operator
+    def test_VOLUME_rebuild(self):
+        data_aug = Volume_Augmentation(flip=False, rotate=False,
+                     brightness=False, contrast=False, saturation=False,
+                     hue=False, scale=False, crop=False, grid_distortion=False,
+                     compression=False, gaussian_noise=False,
+                     gaussian_blur=False, downscaling=False, gamma=False,
+                     elastic_transform=False)
+        img_augRGB = data_aug.apply(self.imgRGB3d)
+        self.assertTrue(np.array_equal(img_augRGB, self.imgRGB3d))
+        data_aug.aug_flip = True
+        data_aug.aug_flip_p = 1.0
+        data_aug.build()
+        img_augRGB = data_aug.apply(self.imgRGB3d)
+        self.assertFalse(np.array_equal(img_augRGB, self.imgRGB3d))

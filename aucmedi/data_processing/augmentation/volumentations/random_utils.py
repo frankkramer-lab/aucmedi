63959a9d287efe7fec99d759fc7b2fac7599a499
muellerdo <domey038@live.de>
2022-02-10 20:13:33 +0100

    Added helper function to Volumentations

---
+++ aucmedi/data_processing/augmentation/volumentations/random_utils.py
@@ -0,0 +1,62 @@
+#=================================================================================#
+#  Author:       Pavel Iakubovskii, ZFTurbo, ashawkey, Dominik Müller             #
+#  Copyright:    albumentations:    : https://github.com/albumentations-team      #
+#                Pavel Iakubovskii  : https://github.com/qubvel                   #
+#                ZFTurbo            : https://github.com/ZFTurbo                  #
+#                ashawkey           : https://github.com/ashawkey                 #
+#                Dominik Müller     : https://github.com/muellerdo                #
+#                2022 IT-Infrastructure for Translational Medical Research,       #
+#                University of Augsburg                                           #
+#                                                                                 #
+#  Volumentations is a subpackage of AUCMEDI, which originated from the           #
+#  following Git repositories:                                                    #
+#       - Original:                 https://github.com/albumentations-team/album  #
+#                                   entations                                     #
+#       - 3D Conversion:            https://github.com/ashawkey/volumentations    #
+#       - Continued Development:    https://github.com/ZFTurbo/volumentations     #
+#       - Enhancements:             https://github.com/qubvel/volumentations      #
+#                                                                                 #
+#  Due to a stop of ongoing development in this subpackage, we decided to         #
+#  integrated Volumentations into AUCMEDI to ensure support and functionality.    #
+#                                                                                 #
+#  MIT License.                                                                   #
+#                                                                                 #
+#  Permission is hereby granted, free of charge, to any person obtaining a copy   #
+#  of this software and associated documentation files (the "Software"), to deal  #
+#  in the Software without restriction, including without limitation the rights   #
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      #
+#  copies of the Software, and to permit persons to whom the Software is          #
+#  furnished to do so, subject to the following conditions:                       #
+#                                                                                 #
+#  The above copyright notice and this permission notice shall be included in all #
+#  copies or substantial portions of the Software.                                #
+#                                                                                 #
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #
+#  SOFTWARE.                                                                      #
+#=================================================================================#
+import numpy as np
+from typing import Optional, Sequence, Union, Type, Any
+import random as py_random
+
+NumType = Union[int, float, np.ndarray]
+IntNumType = Union[int, np.ndarray]
+Size = Union[int, Sequence[int]]
+
+def get_random_state() -> np.random.RandomState:
+    return np.random.RandomState(py_random.randint(0, (1 << 32) - 1))
+
+def randint(
+    low: IntNumType,
+    high: Optional[IntNumType] = None,
+    size: Optional[Size] = None,
+    dtype: Type = np.int32,
+    random_state: Optional[np.random.RandomState] = None,
+) -> Any:
+    if random_state is None:
+        random_state = get_random_state()
+    return random_state.randint(low, high, size, dtype)

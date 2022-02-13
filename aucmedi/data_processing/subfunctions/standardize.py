08f33bd1050b57f0967338fb723a55f8979550b4
muellerdo <domey038@live.de>
2022-02-13 13:54:58 +0100

    Added 3D support and various enhancements for Subfunction: Standardize

--- aucmedi/data_processing/subfunctions/standardize.py
+++ aucmedi/data_processing/subfunctions/standardize.py
@@ -21,22 +21,33 @@
 #-----------------------------------------------------#
 # External libraries
 from tensorflow.keras.applications import imagenet_utils
+import numpy as np
 # Internal libraries/scripts
 from aucmedi.data_processing.subfunctions.sf_base import Subfunction_Base
 
 #-----------------------------------------------------#
 #           Subfunction class: Standardize            #
 #-----------------------------------------------------#
-""" A Standardization method which utilizes the Keras preprocess_input() functionality
-    in order to normalize intensity value ranges to be suitable for neural networks.
+""" A Standardization method which utilizes custom normalization functions and the Keras
+    preprocess_input() functionality in order to normalize intensity value ranges to be
+    suitable for neural networks.
 
-    Default mode: "tf"
-    Possible modes: ["tf", "caffe", "torch"]
+    Default mode: "z-score"
+    Possible modes: ["z-score", "minmax", "grayscale", "tf", "caffe", "torch"]
 
-    Source: https://www.tensorflow.org/api_docs/python/tf/keras/applications/imagenet_utils/preprocess_input
-    caffe: will convert the images from RGB to BGR, then will zero-center each color channel with respect to the ImageNet dataset, without scaling.
-    tf: will scale pixels between -1 and 1, sample-wise.
-    torch: will scale pixels between 0 and 1 and then will normalize each channel with respect to the ImageNet dataset.
+
+Mode Descriptons:
+    Custom Implementations:
+    z-score:    Sample-wise Z-score normalization (also called Z-transformation).
+    minmax:     Sample-wise scaling to range [0,1].
+    grayscale:  Sample-wise scaling to grayscale range [0, 255].
+
+    Keras Implementations: https://www.tensorflow.org/api_docs/python/tf/keras/applications/imagenet_utils/preprocess_input
+    caffe:      Will convert the images from RGB to BGR, then will zero-center each color channel
+                with respect to the ImageNet dataset, without scaling. (RGB encoding required!)
+    tf:         Will scale pixels between -1 and 1, sample-wise. (Grayscale/RGB encoding required!)
+    torch:      Will scale pixels between 0 and 1 and then will normalize each channel with respect
+                to the ImageNet dataset.  (RGB encoding required!)
 
 Methods:
     __init__                Object creation function
@@ -46,14 +57,43 @@ class Standardize(Subfunction_Base):
     #---------------------------------------------#
     #                Initialization               #
     #---------------------------------------------#
-    def __init__(self, mode="tf"):
+    def __init__(self, mode="z-score", smooth=0.000001):
+        # Verify mode existence
+        if mode not in ["z-score", "minmax", "grayscale", "tf", "caffe", "torch"]:
+            raise ValueError("Subfunction - Standardize: Unknown modus", mode)
+        # Cache class variables
         self.mode = mode
+        self.e = smooth
 
     #---------------------------------------------#
     #                Transformation               #
     #---------------------------------------------#
     def transform(self, image):
-        # Perform architecture standardization
-        image_norm = imagenet_utils.preprocess_input(image, mode=self.mode)
+        # Perform z-score normalization
+        if self.mode == "z-score":
+            # Compute mean and standard deviation
+            mean = np.mean(image)
+            std = np.std(image)
+            # Scaling
+            image_norm = (image - mean + self.e) / (std  + self.e)
+        # Perform MinMax normalization between [0,1]
+        elif self.mode == "minmax":
+            # Identify minimum and maximum
+            max_value = np.max(image)
+            min_value = np.min(image)
+            # Scaling
+            image_norm = (image - min_value + self.e) / \
+                         (max_value - min_value + self.e)
+        elif self.mode == "grayscale":
+            # Identify minimum and maximum
+            max_value = np.max(image)
+            min_value = np.min(image)
+            # Scaling
+            image_scaled = (image - min_value + self.e) / \
+                           (max_value - min_value + self.e)
+            image_norm = np.around(image_scaled * 255, decimals=0)
+        else:
+            # Perform architecture standardization
+            image_norm = imagenet_utils.preprocess_input(image, mode=self.mode)
         # Return standardized image
         return image_norm

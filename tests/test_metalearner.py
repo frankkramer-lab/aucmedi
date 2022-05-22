decde329ed8f9bb34e061289840e414821603cf8
muellerdo <domey038@live.de>
2022-05-22 19:05:43 +0200

    test(Metalearner): Added unittesting for Metalearners - Closes #127

---
+++ tests/test_metalearner.py
@@ -0,0 +1,102 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2022 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+#External libraries
+import unittest
+import numpy as np
+import tempfile
+import os
+#Internal libraries
+from aucmedi.ensemble.metalearner import *
+
+#-----------------------------------------------------#
+#                Unittest: Metalearner                #
+#-----------------------------------------------------#
+class MetelearnerTEST(unittest.TestCase):
+    # Create random imaging and classification data
+    @classmethod
+    def setUpClass(self):
+        np.random.seed(1234)
+        # Create model prediction data
+        self.pred_data = np.random.rand(25, 12)
+        # Create classification labels
+        self.labels_ohe = np.zeros((25, 4), dtype=np.uint8)
+        for i in range(0, 25):
+            class_index = np.random.randint(0, 4)
+            self.labels_ohe[i][class_index] = 1
+        # Temporary model directory
+        self.tmp_data = tempfile.TemporaryDirectory(prefix="tmp.aucmedi.",
+                                                    suffix=".model")
+
+    #-------------------------------------------------#
+    #               Logistic Regression               #
+    #-------------------------------------------------#
+    def test_Logistic_Regression_create(self):
+        # Initializations
+        ml = Logistic_Regression()
+        self.assertTrue("logistic_regression" in metalearner_dict)
+        ml = metalearner_dict["logistic_regression"]()
+        # Storage
+        model_path = os.path.join(self.tmp_data.name, "ml_model.pickle")
+        self.assertFalse(os.path.exists(model_path))
+        ml.dump(model_path)
+        self.assertTrue(os.path.exists(model_path))
+        # Loading
+        ml.model = None
+        self.assertTrue(ml.model is None)
+        ml.load(model_path)
+        self.assertFalse(ml.model is None)
+        # Cleanup
+        os.remove(model_path)
+
+    def test_Logistic_Regression_usage(self):
+        # Initializations
+        ml = Logistic_Regression()
+        # Training
+        ml.train(x=self.pred_data, y=self.labels_ohe)
+        # Inference
+        preds = ml.predict(data=self.pred_data)
+        # Check
+        self.assertTrue(np.array_equal(pred.shape, (25,4)))
+
+    # #-------------------------------------------------#
+    # #          Aggregate: Averaging by Median         #
+    # #-------------------------------------------------#
+    # def test_Aggregate_Median(self):
+    #     agg_func = Averaging_Median()
+    #     pred = agg_func.aggregate(self.pred_data.copy())
+    #     self.assertTrue(np.array_equal(pred.shape, (10,)))
+    #
+    # #-------------------------------------------------#
+    # #         Aggregate: Majority Vote (Hard)         #
+    # #-------------------------------------------------#
+    # def test_Aggregate_MajorityVote(self):
+    #     agg_func = Majority_Vote()
+    #     pred = agg_func.aggregate(self.pred_data.copy())
+    #     self.assertTrue(np.array_equal(pred.shape, (10,)))
+    #
+    # #-------------------------------------------------#
+    # #                Aggregate: Softmax               #
+    # #-------------------------------------------------#
+    # def test_Aggregate_Softmax(self):
+    #     agg_func = Softmax()
+    #     pred = agg_func.aggregate(self.pred_data.copy())
+    #     self.assertTrue(np.array_equal(pred.shape, (10,)))

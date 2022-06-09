9cf93d2977683c8419b86951d77ebfcbbc04441e
muellerdo <domey038@live.de>
2022-06-09 23:14:17 +0200

    chore(Package): Update to version 0.6.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("docs/README.PyPI.md", "r") as fh:
 
 setup(
     name='aucmedi',
-    version='0.5.2',
+    version='0.6.0',
     description='AUCMEDI - a framework for Automated Classification of Medical Images',
     author='Dominik Müller',
     author_email='dominik.mueller@informatik.uni-augsburg.de',

d0f4f3a699d1aafac721afd5bbe8b315d3317395
Deathlymad <meyerphilip96@googlemail.com>
2023-06-26 10:29:57 +0200

    feat(dependency): Update setup.py to handle macos

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ setup(
         'console_scripts': ['aucmedi = aucmedi.automl.main:main'],
     },
     python_requires='>=3.8',
-    install_requires=['tensorflow>=2.11.1',
+    install_requires=[
+                      'tensorflow>=2.11.1;sys_platform!="darwin"',
+                      'tensorflow>=2.11.1;sys_platform=="darwin"',
                       'tensorflow-addons>=0.18.0',
                       'keras-applications>=1.0.8',
                       'numpy>=1.23.0',

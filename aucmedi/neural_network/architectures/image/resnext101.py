76866a94ea9d47df6521dc62d6b4d122baa9f9d9
muellerdo <domey038@live.de>
2024-02-21 15:16:33 +0100

    feat(Architecture): added 2D ResNeXt (50 & 101), again...

---
+++ aucmedi/neural_network/architectures/image/resnext101.py
@@ -0,0 +1,82 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2022 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                    Documentation                    #
+#-----------------------------------------------------#
+""" The classification variant of the ResNeXt101 architecture.
+
+| Architecture Variable    | Value                      |
+| ------------------------ | -------------------------- |
+| Key in architecture_dict | "2D.ResNeXt101"            |
+| Input_shape              | (224, 224)                 |
+| Standardization          | "torch"                    |
+
+???+ abstract "Reference - Implementation"
+    [https://github.com/keras-team/keras-applications](https://github.com/keras-team/keras-applications) <br>
+
+???+ abstract "Reference - Publication"
+    Saining Xie, Ross Girshick, Piotr Dollár, Zhuowen Tu, Kaiming He. 16 Nov 2016.
+    Aggregated Residual Transformations for Deep Neural Networks.
+    <br>
+    [https://arxiv.org/abs/1611.05431](https://arxiv.org/abs/1611.05431)
+"""
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+# External libraries
+from tensorflow import keras
+from keras_applications.resnext import ResNeXt101 as BaseModel
+# Internal libraries
+from aucmedi.neural_network.architectures import Architecture_Base
+
+#-----------------------------------------------------#
+#           Architecture class: ResNeXt101            #
+#-----------------------------------------------------#
+class ResNeXt101(Architecture_Base):
+    #---------------------------------------------#
+    #                Initialization               #
+    #---------------------------------------------#
+    def __init__(self, classification_head, channels, input_shape=(224, 224),
+                 pretrained_weights=False):
+        self.classifier = classification_head
+        self.input = input_shape + (channels,)
+        self.pretrained_weights = pretrained_weights
+
+    #---------------------------------------------#
+    #                Create Model                 #
+    #---------------------------------------------#
+    def create_model(self):
+        # Get pretrained image weights from imagenet if desired
+        if self.pretrained_weights : model_weights = "imagenet"
+        else : model_weights = None
+
+        # Obtain ResNeXt101 as base model
+        base_model = BaseModel(include_top=False, weights=model_weights,
+                               input_tensor=None, input_shape=self.input,
+                               pooling=None, backend=keras.backend,
+                               layers=keras.layers, models=keras.models,
+                               utils=keras.utils)
+        top_model = base_model.output
+
+        # Add classification head
+        model = self.classifier.build(model_input=base_model.input,
+                                      model_output=top_model)
+
+        # Return created model
+        return model

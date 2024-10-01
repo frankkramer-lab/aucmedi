81d8db28e41a49ffb09ba31584f8bd13f044a1df
muellerdo <domey038@live.de>
2024-10-01 23:05:51 +0200

    build: updated to version 0.10.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("docs/README.PyPI.md", "r") as fh:
 
 setup(
     name='aucmedi',
-    version='0.9.0',
+    version='0.10.0',
     description='AUCMEDI - a framework for Automated Classification of Medical Images',
     author='Dominik Müller',
     author_email='dominik.mueller@informatik.uni-augsburg.de',

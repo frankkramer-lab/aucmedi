891c6730ec71e17c6e55dd22f06520e71592a4c7
wehrfabi <fabian.wehr.uni-a.de>
2026-08-19 14:40:05 +0200

    docs: updated license year

--- aucmedi/neural_network/architectures/volume/densenet121.py
+++ aucmedi/neural_network/architectures/volume/densenet121.py
@@ -1,6 +1,6 @@
-# ==============================================================================#
+﻿# ==============================================================================#
 #  Author:       Dominik Müller                                                #
-#  Copyright:    2024 IT-Infrastructure for Translational Medical Research,    #
+#  Copyright:    2026 IT-Infrastructure for Translational Medical Research,    #
 #                University of Augsburg                                        #
 #                                                                              #
 #  This program is free software: you can redistribute it and/or modify        #
